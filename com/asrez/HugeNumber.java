9da29fab4e86157f18f160e9c5bdfc90d6054af7
Max Base <Max@Asrez.com>
2019-12-09 20:49:44 +0330

    Do/Write some bad and bug code...

--- com/asrez/HugeNumber.java
+++ com/asrez/HugeNumber.java
@@ -6,127 +6,128 @@ import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import java.util.ArrayList;
 
 public class HugeNumber {
-//    public int length;// We can use 'long'...
-    public int[] array;
-//    private ArrayList<Integer> array = new ArrayList<>();
+	public int[] array;
 
-    HugeNumber(String input) {
-        array=new int[input.length()];
-        int i;
-        for(i=0;i<input.length();i++) {
-            if(Character.isDigit(input.charAt(i))) {
-//                array.add(Integer.parseInt(Character.toString(input.charAt(i))));
-                array[i]=Integer.parseInt(Character.toString(input.charAt(i)));
-            }
-            else {
-//                throw new Exception("The input is not valid!");
-                break;
-            }
-        }
-//        length=i;
-    }
+	HugeNumber(String input) {
+		if(input == null || input.length() == 0) {
+			System.err.println("Error: input of constructor is empty!");
+			return;
+		}
+		array=new int[input.length()];
+		int i;
+		for(i=input.length()-1;i>=0;i--) {
+			if(Character.isDigit(input.charAt(i))) {
+				array[i]=Integer.parseInt(Character.toString(input.charAt(i)));
+			}
+			else {
+				break;
+			}
+		}
+	}
 
-    HugeNumber(HugeNumber input) {
-        this.array=input.array;
-    }
+	HugeNumber(HugeNumber input) {
+		this.array=input.array;
+	}
 
-    HugeNumber(int input) {
-//        for(int i=getLengthOfNumber(input)-1;i>=0;i--) {}
-//        while(input != 0) {
-////            System.out.println(input % 10);
-//            array.add(input % 10);
-//            input /= 10;
-//        }
-//        for(int i=getLength(input)-1;input != 0;i--) {
-//            array[i]=input % 10;
-//            input /= 10;
-//        }
-        int i=0;
-        int inputLength=getLength(input);
-        array=new int[inputLength];
-        while(input != 0) {
-            array[inputLength - ++i]=(input % 10);
-            input /= 10;
-        }
-//        array=reverseArrayList();
-    }
+	HugeNumber(int input) {
+		if(input < 0) {
+			System.err.println("Error: input of constructor is not valid!");
+		}
+		int inputLength=getLength(input);
+		array=new int[inputLength];
+		int i=0;
+		while(input != 0) {
+			array[i++]=(input % 10);
+			input /= 10;
+		}
+	}
 
-    public int getLength(int input) {
-        int i=0;
-        while(input != 0) {
-            i++;
-            input/=10;
-        }
-        return i;
-    }
+	public int getLength(int input) {
+		int i=0;
+		while(input != 0) {
+			i++;
+			input/=10;
+		}
+		return i;
+	}
 
-    public int getLength() {
-        return array.length;
-    }
+	public int getLength() {
+		return array.length;
+	}
 
-//    public ArrayList<Integer> getArray() {
-//        return array;
-//    }
-    public int[] getArray() {
-        return array;
-    }
+	public int[] getArray() {
+		return array;
+	}
 
-//    public ArrayList<Integer> reverseArrayList() {
-//        ArrayList<Integer> revArrayList = new ArrayList<Integer>();
-//        for (int i = array.length - 1; i >= 0; i--) {
-//            revArrayList.add(array[i]);
-//        }
-//        return revArrayList;
-//    }
+	public void print() {
+		print(true);
+	}
 
-    public void print() {
-//        for(int i = 0; i < array.size(); i++) {
-//            System.out.print(array.get(i) + " ");
-//        }
-        for(int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
-    }
+	public void print(boolean split) {
+		if(split) {
+			for(int i = array.length-1; i >= 0; i--) {
+				System.out.print(array[i] + " ");
+			}
+		}
+		else {
+			for(int i = array.length-1; i >= 0; i--) {
+				System.out.print(array[i]);
+			}
+		}
+		System.out.println();
+	}
 
-    private static int[] resizeArray(int[] array, int size) {
-        return resizeArray(array, size);
-    }
+	public String result() {
+		String result="";
+		for(int i = array.length-1; i >= 0; i--) {
+			result+=array[i];
+		}
+		return result;
+	}
 
-    private static int[] resizeArray(int[] array, int size,boolean copy) {
-        int newArray[] = new int[size];
-        if(copy) {
-            System.arraycopy(array, 0, newArray, 0, size);
-        }
-        return newArray;
-    }
+	public String __toString() {
+		return result();
+	}
 
-    //Add +
-    public void add(HugeNumber input) {
-        int max= Integer.max(input.array.length, array.length);
-        for(int i=0;i<input.array.length;i++) {
-            array[max-1-i]=array[max-1-i] + input.array[input.array.length-1-i];
-            for(int j=0;array[max-1-i-j] >= 10;j++) {
-                array[max-1-i-j]=0;
-                array[max-1-i-1-j]=array[max-1-i-1-j] + 1;
-            }
-        }
-    }
+	private static int[] resizeArray(int[] array, int size, int to) {
+		return resizeArray(array, size, to, true);
+	}
 
-    //Subtract -
-    //Multiplie *
-    //Divide /
-    //Remainder %
+	private static int[] resizeArray(int[] array, int size, int to, boolean copy) {
+		int newArray[] = new int[size];
+		if(copy) {
+			System.arraycopy(array, 0, newArray, 0, size);
+		}
+		return newArray;
+	}
 
-    public static void main(String arguments[]) {
-        HugeNumber test=new HugeNumber(1398);
-        System.out.println(test.getArray().toString());
-        System.out.println(test.getLength());
-        System.out.println(test.getLength(1398));
-        test.print();
-        test.add(new HugeNumber("110"));
-        test.add(new HugeNumber("110"));
-        test.print();
-    }
+	//Add +
+	public void add(HugeNumber input) {
+		int max= Integer.max(input.array.length, array.length);
+		array=resizeArray(array, max, array.length-1);
+		for(int i=0;i<input.array.length; i++) {
+			array[i]+=input.array[i];
+			// for(int j=0;array[max-1-i-j] >= 10;j++) {
+			// 	array[max-1-i-j]=0;
+			// 	array[max-1-i-1-j]=array[max-1-i-1-j] + 1;
+			// }
+		}
+	}
+
+	//Subtract -
+	//Multiplie *
+	//Divide /
+	//Remainder %
+
+	public static void main(String arguments[]) {
+		HugeNumber test=new HugeNumber(1398);//8931
+		// System.out.println(test.getLength());
+		test.print(false);
+		test.add(new HugeNumber("110"));//011
+		// test.print(false);
+		// test.add(new HugeNumber("110"));
+		// test.print(false);
+		// test.add(new HugeNumber("50000"));
+		test.print(false);
+	}
 }
-
