a982998ea2940499bba8a6c96cc45326a08db447
Max Base <Max@Asrez.com>
2019-12-09 21:07:01 +0330

    Create Add method

--- com/asrez/HugeNumber.java
+++ com/asrez/HugeNumber.java
@@ -6,18 +6,22 @@ import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import java.util.ArrayList;
 
 public class HugeNumber {
-	public int[] array;
+	public ArrayList<Integer> array=new ArrayList<Integer>();
 
+
+	public void clear() {
+		array=new ArrayList<Integer>();
+	}
 	HugeNumber(String input) {
 		if(input == null || input.length() == 0) {
 			System.err.println("Error: input of constructor is empty!");
 			return;
 		}
-		array=new int[input.length()];
 		int i;
+		clear();
 		for(i=input.length()-1;i>=0;i--) {
 			if(Character.isDigit(input.charAt(i))) {
-				array[i]=Integer.parseInt(Character.toString(input.charAt(i)));
+				array.add(Integer.parseInt(Character.toString(input.charAt(i))));
 			}
 			else {
 				break;
@@ -34,10 +38,9 @@ public class HugeNumber {
 			System.err.println("Error: input of constructor is not valid!");
 		}
 		int inputLength=getLength(input);
-		array=new int[inputLength];
-		int i=0;
+		clear();
 		while(input != 0) {
-			array[i++]=(input % 10);
+			array.add(input % 10);
 			input /= 10;
 		}
 	}
@@ -52,10 +55,10 @@ public class HugeNumber {
 	}
 
 	public int getLength() {
-		return array.length;
+		return array.size();
 	}
 
-	public int[] getArray() {
+	public ArrayList getArray() {
 		return array;
 	}
 
@@ -65,13 +68,13 @@ public class HugeNumber {
 
 	public void print(boolean split) {
 		if(split) {
-			for(int i = array.length-1; i >= 0; i--) {
-				System.out.print(array[i] + " ");
+			for(int i = array.size()-1; i >= 0; i--) {
+				System.out.print(array.get(i) + " ");
 			}
 		}
 		else {
-			for(int i = array.length-1; i >= 0; i--) {
-				System.out.print(array[i]);
+			for(int i = array.size()-1; i >= 0; i--) {
+				System.out.print(array.get(i));
 			}
 		}
 		System.out.println();
@@ -79,8 +82,8 @@ public class HugeNumber {
 
 	public String result() {
 		String result="";
-		for(int i = array.length-1; i >= 0; i--) {
-			result+=array[i];
+		for(int i = array.size()-1; i >= 0; i--) {
+			result+=array.get(i);
 		}
 		return result;
 	}
@@ -89,28 +92,25 @@ public class HugeNumber {
 		return result();
 	}
 
-	private static int[] resizeArray(int[] array, int size, int to) {
-		return resizeArray(array, size, to, true);
-	}
-
-	private static int[] resizeArray(int[] array, int size, int to, boolean copy) {
-		int newArray[] = new int[size];
-		if(copy) {
-			System.arraycopy(array, 0, newArray, 0, size);
-		}
-		return newArray;
-	}
-
 	//Add +
 	public void add(HugeNumber input) {
-		int max= Integer.max(input.array.length, array.length);
-		array=resizeArray(array, max, array.length-1);
-		for(int i=0;i<input.array.length; i++) {
-			array[i]+=input.array[i];
-			// for(int j=0;array[max-1-i-j] >= 10;j++) {
-			// 	array[max-1-i-j]=0;
-			// 	array[max-1-i-1-j]=array[max-1-i-1-j] + 1;
-			// }
+		int max= Integer.max(input.array.size(), array.size());
+		for(int i=0;i<input.array.size(); i++) {
+			if(i >= array.size()) {
+				array.add(input.array.get(i));
+			}
+			else {
+				array.set(i, (int)array.get(i) + (int)input.array.get(i));
+			}
+			for(int j=0;array.get(i+j) >= 10;j++) {
+				array.set(i+j, 0);
+				if(i+j+1 >= array.size()) {
+					array.add(1);
+				}
+				else {
+					array.set(i+j+1, array.get(i+j+1) + 1);
+				}
+			}
 		}
 	}
 
@@ -120,14 +120,14 @@ public class HugeNumber {
 	//Remainder %
 
 	public static void main(String arguments[]) {
-		HugeNumber test=new HugeNumber(1398);//8931
+		HugeNumber test=new HugeNumber(1398+50000);//8931
 		// System.out.println(test.getLength());
 		test.print(false);
 		test.add(new HugeNumber("110"));//011
 		// test.print(false);
 		// test.add(new HugeNumber("110"));
 		// test.print(false);
-		// test.add(new HugeNumber("50000"));
+		test.add(new HugeNumber("50000"));
 		test.print(false);
 	}
 }
